914fca44a94258ecb808c4926d6adf4778a3d97b
ZZangHo <lineaheshsh@naver.com>
2018-05-23 17:36:13 +0900

    Vuejs + SpringBoot Start

---
+++ src/main/java/com/boot/spring/VueSpringApplication.java
@@ -0,0 +1,12 @@
+package com.boot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringApplication.class, args);
+	}
+}
